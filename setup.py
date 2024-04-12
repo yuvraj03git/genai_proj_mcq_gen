487252c98487ce6bcc705960773328e63013b767
yuvraj03git <97453837+yuvraj03git@users.noreply.github.com>
2024-04-12 12:02:51 +0000

    new1

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
  
+ name='mcqgenerator',
+ version='0.0.1',
+ author='Yuvraj Shivam',
+ author_email="iib2021006@iiita.ac.in",
+ install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+ packages=find_packages()
+
+)
